fbf15f36e67e9a6e096e48a6b1d36ad8aa55b4b9
fischundfleisch <94743049+fischundfleisch@users.noreply.github.com>
2021-12-21 14:43:03 +0100

    Update pumpcontrol.ino
    paar neue Fehler rein geschrieben. 
    button druck begonnen. Timeout begonnen.

--- pumpcontrol.ino
+++ pumpcontrol.ino
@@ -7,9 +7,13 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int TRIGGER = 7;
 const int ECHO = 6;
 const int DISPLAY_ON = 13;
+const int BUTTON_1 = 8;
+const int BUTTON_2 = 9;
 const unsigned long SCAN_FREQ = 10UL*1000UL;
+const unsigned long DISPLAY_SLEEP = 1UL * 60UL *1000UL;
 
 unsigned long scan_timer_ =0;
+unsigned long sleep_timer_ = 0;
 
 void setup() {
   pinMode(DISPLAY_ON, OUTPUT);
@@ -17,19 +21,42 @@ void setup() {
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
   digitalWrite(ECHO, LOW);
+  pinMode(BUTTON_1, INPUT);
+  digitalWrite(BUTTON_1, HIGH);
+  pinMode(BUTTON_2, INPUT);
+  digitalWrite(BUTTON_2, HIGH);
   
   lcd.begin(16, 2);  //16 Zeichen, 2 Zeilen
   lcd.setCursor(0,0); //Position Zeichen 0, Zeile 0
   lcd.print("Willkommen!");
   Serial.begin(9600);
-  delay(2000);
+  delay(1000);
 }
 
 void loop() {
   unsigned long act_time = millis();
   unsigned long time_span = act_time - scan_timer_;
+  unsigned long timespan_display = act_time - sleep_timer_;
+  
   long duration = 0;
   long distance = 0;
+
+  int button_state = 0;
+
+
+button_state = digitalRead(BUTTON_1);
+Serial.println(button_state);
+  if (button_state == LOW) {
+    digitalWrite(DISPLAY_ON, HIGH);
+    lcd.setCursor(0,0);
+    lcd.print("Pumpe 1 an");
+    sleep_timer_ = millis();
+    Serial.println("Button gedrückt");
+  }
+
+  if (timespan_display > DISPLAY_SLEEP){
+    digitalWrite(DISPLAY_ON, LOW);  //schaltet Display ab
+  }
 
   if (time_span > SCAN_FREQ) {
     digitalWrite(TRIGGER, LOW);
@@ -44,7 +71,7 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print(distance);
     lcd.print(" cm");
-    scan_timer_ = (millis);
+    scan_timer_ = millis();
   }
 
   }
